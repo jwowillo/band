a4513b8c2ea5426fa46fd0ab207ee3ff1cc02be7
jwowillo <will.orourke10@gmail.com>
2020-08-08 09:41:43 -0500

    Provide a Factory that Sets Default Style Overrides

--- band/all.h
+++ band/all.h
@@ -1,4 +1,5 @@
 #pragma once
 
 #include "band/control.h"
+#include "band/control_factory.h"
 #include "band/interface.h"
---
+++ band/control_factory.cc
@@ -0,0 +1,7 @@
+#include "band/control_factory.h"
+
+namespace band {
+
+ControlFactory::ControlFactory(const Style& style) : style_{style} { }
+
+}  // namespace band
